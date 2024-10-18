126e3fa99fc067f2f4e35cb6a1497e7408c9e3fe
NightmareZX <zsolt.toldi03@gmail.com>
2024-10-18 10:47:20 +0100

    AudioAnalyzer is still bad but fine for demo

--- AudioAnalyzer.cs
+++ AudioAnalyzer.cs
@@ -3,9 +3,11 @@ using NAudio.Wave;
 using Spectrogram;
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Complex = NAudio.Dsp.Complex;
 
 namespace WinForms_TestApp
 {
@@ -16,19 +18,26 @@ namespace WinForms_TestApp
         public readonly SpectrogramGenerator generator;
         private readonly int fftSize = 2048;  // Size for FFT
         private float[] previousByteData; // Stores previous data for smoothing
-        private readonly Complex[] fftBuffer;
         private readonly float[] audioBuffer;
         private int bufferIndex = 0;
         private float smoothingTimeConstant;
+        private object bufferlock = new();
+        private float[] hannWindow;
+        private Queue<Complex[]> dataQueue = new();
+        private Complex[] incompleteData;
+        private Complex[] lastData;
+        private int incompleteDataIndex;
 
         public AudioAnalyzer(ISampleProvider source, BufferedWaveProvider buffer)
         {
             this.source = source;
             this.buffer = buffer;
-            fftBuffer = new Complex[fftSize];
             audioBuffer = new float[fftSize];
             previousByteData = new float[fftSize / 2];
             generator = new(source.WaveFormat.SampleRate, fftSize, 500);
+            hannWindow = GenerateHannWindow();
+            lastData = new Complex[fftSize];
+            incompleteDataIndex = -1;
         }
 
         public WaveFormat WaveFormat => source.WaveFormat;
@@ -36,22 +45,61 @@ namespace WinForms_TestApp
         public int Read(float[] buffer, int offset, int count)
         {
             // Read audio data from the source
-            int samplesRead = source.Read(buffer, offset, count);
-            generator.Add(Array.ConvertAll(buffer, x => (double)x), false);
-            // Write the audio data to the buffer (for playback)
-            for (int i = 0; i < samplesRead; i++)
+            lock (bufferlock)
             {
-                if (bufferIndex < fftSize)
+                int samplesRead = source.Read(buffer, offset, count);
+                //generator.Add(Array.ConvertAll(buffer, x => (double)x), false);
+                // Write the audio data to the buffer (for playback)
+                int dataIndex = 0;
+                Complex[] fftDataToAdd;
+                if (incompleteDataIndex != -1)
                 {
-                    audioBuffer[bufferIndex] = buffer[i + offset];
-                    bufferIndex++;
+                    fftDataToAdd = incompleteData;
+                    dataIndex = incompleteDataIndex;
                 }
-                if (bufferIndex >= fftSize)
+                else
                 {
-                    bufferIndex = 0;  // Reset buffer index
+                    fftDataToAdd = new Complex[fftSize];
                 }
+                incompleteDataIndex = -1;
+                for (int i = 0; i < samplesRead; i++)
+                {
+                    fftDataToAdd[dataIndex].X = buffer[i];
+
+                    dataIndex++;
+                    if (i == samplesRead - 1 && dataIndex != fftSize)
+                    {
+                        incompleteDataIndex = dataIndex;
+                        incompleteData = fftDataToAdd;
+                    }
+                    else if (i != samplesRead - 1 && dataIndex >= fftSize)
+                    {
+                        dataQueue.Enqueue(fftDataToAdd);
+                        fftDataToAdd = new Complex[fftSize];
+                        dataIndex = 0;
+                    }
+                    //if (bufferIndex < fftSize)
+                    //{
+                    //    audioBuffer[bufferIndex] = buffer[i + offset];
+                    //    bufferIndex++;
+                    //}
+                    //if (bufferIndex >= fftSize)
+                    //{
+                    //    bufferIndex = 0;  // Reset buffer index
+                    //}
+                }
+                return samplesRead;
             }
-            return samplesRead;
+        }
+        private float[] GenerateHannWindow()
+        {
+            float[] window = new float[fftSize / 2];
+            float angleUnit = 2 * MathF.PI / (window.Length - 1);
+            for (int i = 0; i < window.Length; i++)
+            {
+                window[i] = 0.5f * (1 - MathF.Cos(i * angleUnit));
+            }
+            return window;
         }
         private void ApplySmoothing(float[] currentData)
         {
@@ -62,15 +110,34 @@ namespace WinForms_TestApp
         }
         public byte[] GetByteFrequencyData()
         {
-            var spec = generator.Process();
+            //var spec = generator.Process();
             float[] magnitudes = new float[fftSize / 2];
             // Copy audio data into the FFT buffer
-            for (int i = 0; i < fftSize; i++)
+            Complex[] fftBuffer;
+            int currentCount;
+            lock (bufferlock)
             {
-                fftBuffer[i].X = (float)(audioBuffer[i] * FastFourierTransform.HannWindow(i, fftSize)); // Apply window function
-                fftBuffer[i].Y = 0;
+                if(dataQueue.Count > 0)
+                {
+                    fftBuffer = dataQueue.Dequeue();
+                }
+                else
+                {
+                    fftBuffer = lastData;
+                }
+                currentCount = dataQueue.Count;
+            }
+            if(currentCount == 0)
+            {
+                fftBuffer.CopyTo(lastData, 0);
             }
 
+            for (int i = 0; i < fftSize; i++)
+            {
+                fftBuffer[i].X *= (float)FastFourierTransform.HannWindow(i, fftSize); // Apply window function
+                //fftBuffer[i].X = (float)(audioBuffer[i] * FastFourierTransform.HannWindow(i, fftSize)); // Apply window function
+                //fftBuffer[i].Y = 0;
+            }
             // Perform FFT
             FastFourierTransform.FFT(true, (int)Math.Log(fftSize, 2), fftBuffer);
 
